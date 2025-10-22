8612ba18406a5c9887efb7d5bf83942be6d716c7
medeepakmishra <146239205+medeepakmishra@users.noreply.github.com>
2025-10-22 22:39:53 +0530

    2nd commit

--- JAVA Lec-2/teneryOperator.java
+++ JAVA Lec-2/teneryOperator.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class teneryOperator {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a  = sc.nextInt();
 
+        System.out.println((a%2==0)?"Even":"odd");
     }
 }
