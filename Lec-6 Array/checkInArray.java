b994e6bcc511f85e6d7c97afbf915f72350abf54
medeepakmishra <146239205+medeepakmishra@users.noreply.github.com>
2026-06-25 00:28:50 +0530

    ..

---
+++ Lec-6 Array/checkInArray.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class checkInArray {
+    public static void main(String[] args) {
+        
+        int []arr = { 1,2,4,5,6,7,8,8};
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number to check it is present in array or not ");
+        int n = sc.nextInt();
+        boolean found = false;
+
+        for (int i = 0; i < arr.length; i++) {
+            if(n==arr[i]){
+               found = true;
+
+            } 
+           
+        }
+
+        if(found==true){
+            System.out.println("Element is present in array");
+        }else{
+            System.out.println("Element is not present in array");
+        }
+    };
+    
+}
--- Lec-6 Array/question_01.java
+++ Lec-6 Array/question_01.java
@@ -2,7 +2,29 @@ public class question_01 {
 
      public static void main(String[] args){
         // multiply odd index by 2 and add 10 to the even index 
-        
+
+        int [] arr = {9,8,7,6,5,4,3,2,1};
+
+        for (int i = 0; i < arr.length; i++) {
+            
+            if(i%2==0){
+                arr[i]= arr[i]*2;
+            }
+            else{
+                arr[i] = arr[i]+ 10;
+            }
+           
+          
+          System.out.println(arr[i]);
+          System.out.println();
+        }
+      
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+            
+        }
+
+
 
       
      }
